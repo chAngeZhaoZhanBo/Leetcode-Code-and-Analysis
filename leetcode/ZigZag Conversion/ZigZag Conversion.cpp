6ceb4a826795f28a8befa2a1cdab21770c1b4bbd
chAnge1026 <50198008+chAngeZhaoZhanBo@users.noreply.github.com>
2019-07-21 20:09:58 +0800

    Update ZigZag Conversion.cpp

--- leetcode/ZigZag Conversion/ZigZag Conversion.cpp
+++ leetcode/ZigZag Conversion/ZigZag Conversion.cpp
@@ -1,1 +1,22 @@
-
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if(numRows==1)
+            return s;
+        string str;
+        const int len=2*numRows-2;
+        int cur,inc;
+        for(int i=0;i<numRows;++i){
+            cur=i;
+            inc=len-2*i;
+            while(cur<s.size()){
+                if(inc){
+                    str+=s[cur];
+                }
+                cur+=inc;
+                inc=len-inc;
+            }
+        }
+        return str;
+    }
+};
